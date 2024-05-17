584b93535c022b4eb299471d921c42cb15b6bd00
Estevan <egonz94@gmail.com>
2024-05-17 15:37:07 -0500

    Added pucks

--- game.js
+++ game.js
@@ -7,10 +7,10 @@ function preload () {
 }
 
 function create () {
-	for (let y = 0; y < 8; y ++) {
 
+	//Draw Checkers Board
+	for (let y = 0; y < 8; y ++) {
 		for (let x = 0; x < 8; x += 2) {
-			
 			if (y % 2 === 0) {
 				this.add.rectangle((x * 100), (y * 100), 100, 100, 0x000000).setOrigin(0,0)
 				this.add.rectangle((x * 100) + 100, (y * 100), 100, 100, 0xffffff).setOrigin(0,0)
@@ -18,12 +18,29 @@ function create () {
 				this.add.rectangle((x * 100), (y * 100), 100, 100, 0xffffff).setOrigin(0,0)
 				this.add.rectangle((x * 100) + 100, (y * 100), 100, 100, 0x000000).setOrigin(0,0)
 			}
-
-
 		}
 	}
 
+	//Draw Pucks
+	for (x=150; x < 1000; x += 200) {
+		this.add.circle(x, 50, 40, 0x0000ff)
+	}
+	for (x=50; x < 1000; x += 200) {
+		this.add.circle(x, 150, 40, 0x0000ff)
+	}
+	for (x=150; x < 1000; x += 200) {
+		this.add.circle(x, 250, 40, 0x0000ff)
+	}
 
+	for (x=150; x < 1000; x += 200) {
+		this.add.circle(x, 550, 40, 0xff0000)
+	}
+	for (x=50; x < 1000; x += 200) {
+		this.add.circle(x, 650, 40, 0xff0000)
+	}
+	for (x=150; x < 1000; x += 200) {
+		this.add.circle(x, 750, 40, 0xff0000)
+	}
 }
 
 function update () {
